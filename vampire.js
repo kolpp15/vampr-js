82f9d7301622e56b684f3216986570564b52328b
Brian Sohn <kolpp15@gmail.com>
2021-08-06 23:01:03 -0700

    vampire app class functions

--- vampire.js
+++ vampire.js
@@ -6,26 +6,52 @@ class Vampire {
     this.creator = null;
   }
 
-  /** Simple tree methods **/
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+
+  }
+
+
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,8 +62,9 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    
   }
+  
 }
 
 module.exports = Vampire;
